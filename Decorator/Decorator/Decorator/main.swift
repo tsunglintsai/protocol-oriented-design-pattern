9101a5cf4e12d7a3b9e14ef9883e122719acea7e
Henry Tsai <tsunglin.tsai@gmail.com>
2017-04-06 15:16:40 -0700

    Added Decorate Project

---
+++ Decorator/Decorator/Decorator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Decorator
+//
+//  Created by Henry on 4/6/17.
+//  Copyright © 2017 Pyrogusto Inc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
